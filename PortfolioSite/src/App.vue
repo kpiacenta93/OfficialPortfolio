375fcbb54a2925f16813286c463f206e27399562
Kyle Piacenta <124377155+kpiacenta93@users.noreply.github.com>
2024-01-14 14:20:38 -0500

    first commit for Official portfolio

---
+++ PortfolioSite/src/App.vue
@@ -0,0 +1,45 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <nav class="navbar">
+    <ul>
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/projects">Projects</router-link></li>
+      <li><router-link to="/education">Education</router-link></li>
+      <li><router-link to="/experience">Experience</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
+  </nav>
+  </div>
+  
+</template>
+
+<style scoped>
+.navbar ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  background-color: #333;
+}
+
+.navbar li {
+  float: left;
+}
+
+.navbar li a {
+  display: block;
+  color: white;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+}
+
+/* Change the link color to #111 (black) on hover */
+.navbar li a:hover {
+  background-color: #111;
+}
+</style>
